6d4798cb0629e29c04781bb96d42bdde936e4b42
Joan Sebastian Velandia G <joan_velandia@javeriana.edu.co>
2020-11-15 17:18:14 -0500

    avance reproduccion video

--- VideoCode/src/GUIController.java
+++ VideoCode/src/GUIController.java
@@ -9,19 +9,38 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.media.MediaPlayer.Status;
 import javafx.stage.FileChooser;
+import java.time.Duration;
 
 public class GUIController {
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML // urlVideo location of the FXML file that was given to the FXMLLoader
     private URL location;
 
     @FXML
+    private Slider reproductionTime;
+
+    @FXML
     private Slider navBarVolume;
-    
+
+    @FXML
+    private MediaView mediaView;
+
+
+
+    Media media;
+    MediaPlayer mediaPlayer;
+    boolean isPlaying = false;
+
+    private Number value;
+
     @FXML
     void addPicture(MouseEvent event) {
         System.out.println("subir imagen");
@@ -33,26 +52,47 @@ public class GUIController {
     }
 
     @FXML
-    void addVideo(MouseEvent event){
-        
+    void addVideo(MouseEvent event) {
+
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Seleccionar Vídeo");
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("*.mp4", "*.mkv"));
-        String url = "";
+        fileChooser.getExtensionFilters()
+                .addAll(new FileChooser.ExtensionFilter("Vídeo (*.mp4, *.mkv)", "*.mp4", "*.mkv"));
+        String urlVideo = "";
         File file = fileChooser.showOpenDialog(null);
         if (file != null) {
             System.out.println(file.getPath());
             String rutaVideo = file.getPath();
-            url = "file://" + file.getPath();
+            urlVideo = "file:///" + file.getPath();
+            urlVideo = "file:///D:/videoTest.mp4";
+            System.out.println("subir video " + rutaVideo);
+
         } else {
-            System.out.println(url);
+            System.out.println(urlVideo);
         }
-        
-        //System.out.println("subir video");
-        //this.navBarVolume.setMin(0);
-        //this.navBarVolume.setMax(1);
-        //this.navBarVolume.setValue(1);
+        media = new Media(urlVideo);
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+
+        mediaPlayer.setOnReady( () -> {
+            reproductionTime.setMin(0);
+            reproductionTime.setMax(mediaPlayer.getTotalDuration().toSeconds());
+            reproductionTime.setValue(0);
+            mediaPlayer.play();
+        });
+        reproductionTime.valueProperty().addListener(
+            (paramet, observer, value) -> {​​​​
+                //GUIController.this.value = value;
+                reproductionTime.setValue((double) GUIController.this.value);
+            }​​​
+        );
+        this.isPlaying = true;
+
+        navBarVolume.setMin(0);
+        navBarVolume.setMax(1);
+        navBarVolume.setValue(1);
+        mediaPlayer.volumeProperty().bind(navBarVolume.valueProperty());
+
     }
 
     @FXML
@@ -68,12 +108,34 @@ public class GUIController {
     @FXML
     void muteClick(MouseEvent event) {
         System.out.println("mute");
-        //this.navBarVolume.setValue(0);
+        // this.navBarVolume.setValue(0);
     }
 
     @FXML
     void playClick(MouseEvent event) {
-        System.out.println("play");
+        Status status = this.mediaPlayer.getStatus();
+
+        if (status == Status.UNKNOWN || status == Status.HALTED)
+        {
+            // don't do anything in these states
+            return;
+        }
+
+        if (status == Status.PAUSED || status == Status.READY || status == Status.STOPPED)
+        {
+            // rewind the movie if we're sitting at the end
+            if (!this.isPlaying) 
+            {
+                this.mediaPlayer.seek(this.mediaPlayer.getStartTime());
+                this.isPlaying = true;
+            }
+            System.out.println("play");
+            this.mediaPlayer.play();
+        } else 
+        {
+            System.out.println("pause");
+            this.mediaPlayer.pause();
+        }
     }
 
     @FXML
